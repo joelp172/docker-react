fe62e2b959a7b0d2c650c8dc50702c10ed52aef3
System Administrator <root@Joels-MacBook-Pro.local>
2021-10-11 21:10:46 +0100

    new text 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Feature Branch dd
+          Feature Branch
         </a>
       </header>
     </div>
